9c75b496a903b6ad6871ebd50351c17940355277
Armin Pfarr <armin.pfarr@gmail.com>
2017-02-15 10:39:03 +0100

    Added configuration class

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-app/src/main/java/__service-name__Config.java
@@ -0,0 +1,24 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * 
+ */
+package ${package};
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This class holds the configuration for microservice ${service-name}
+ * @author arp
+ *
+ */
+@Configuration
+public class ${service-name}Config {
+	
+	private static Logger logger = LoggerFactory.getLogger(${service-name}Config.class);
+	
+}
